ad83547bfcf63df5764f11920f171786181ba149
utente <mario.manfredi64@gmail.com>
2026-06-13 11:08:54 +0200

    Inseriti array

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
